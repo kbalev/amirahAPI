0d8b7648e1a4d379744198f6f71a6e4b94108d8b
kbalev <kamen_balev@yahoo.com>
2024-06-26 22:05:26 +0100

    lets give it a go

--- src/.next/server/server-reference-manifest.js
+++ src/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MpgOPVJFiY/BLyevxAvm9mfxSgTufj2mVNs4tR5ILbU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"57y9lpGxvVQLijLG4zZjPhBj/bBsxY983qC6Z03cgw4=\"\n}"
